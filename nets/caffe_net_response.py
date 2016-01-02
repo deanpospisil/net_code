c07bed758f3e0049bcd842d7f0a0910496115948
dean <deanp3@uw.edu>
2016-01-02 14:28:24 -0800

    a little commenting on net response

--- nets/caffe_net_response.py
+++ nets/caffe_net_response.py
@@ -12,6 +12,7 @@ import os
 import sys
 import warnings
 
+#make the working directory two above this one
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 cwd = os.path.dirname(dname)
@@ -73,8 +74,9 @@ def identity_preserving_transform_resp( img_stack, stim_specs_dict, net, nimgs_p
 
 def stim_idprestrans_generator(shape = None, scale = None, x = None, y = None, 
                         rotation = None):
-    #either makes a ragged dict lists using nans, or makes a full cartesian 
-    #product dictionary
+# takes descrptions of ranges for different transformations (start, stop, npoints)
+#produces dictionary of those.
+                        
     stim_specs_dict = ordDict()                 
         
     if not shape is None:
@@ -100,6 +102,7 @@ def stim_idprestrans_generator(shape = None, scale = None, x = None, y = None,
     return stim_trans_dict
          
 def cartesian_prod_dicts_lists( the_dict ) :
+    #takes a dictionary and produces a dictionary of the cartesian product of the input
     
     if not type(the_dict) is type(ordDict()):
         warnings.warn('We were expecting an ordered dict for provenance concerns.')
@@ -125,28 +128,33 @@ def cartesian_prod_dicts_lists( the_dict ) :
 #d['b'] = [1,2]
 #c_d = cartesian_prod_dicts_lists( d )
 
-def argsort(seq):
-    # http://stackoverflow.com/questions/3071415/efficient-method-to-calculate-the-rank-vector-of-a-list-in-python
-    return sorted(range(len(seq)), key=seq.__getitem__)
-    #
-    
+#def argsort(seq):
+#    # http://stackoverflow.com/questions/3071415/efficient-method-to-calculate-the-rank-vector-of-a-list-in-python
+#    return sorted(range(len(seq)), key=seq.__getitem__)
+#    #
+#    
+
+
 def load_sorted_dir_numbered_fnms_with_particular_extension(the_dir, extension):
-      
+    #takes a directory and an extension, and loads up all file names with that extension,
+    #attempting to sort thm by number, gives back all the sorted file names
     dir_filenames = os.listdir(the_dir)
     file_names = [ file_name for file_name in dir_filenames if file_name.split('.')[-1] == extension ]
     
     file_names = sorted( file_names, key = lambda num : int(num.split('.')[0]) )
     
     return file_names
+    
 
 def load_npy_img_dirs_into_stack( img_dir ):
+    #given a directory, loads all the npy images in it, into a stack.
     
     stack_descriptor_dict = {}
     img_names = load_sorted_dir_numbered_fnms_with_particular_extension( img_dir , 'npy')
     
     #will need to check this for color images.
-    stack = np.array([ np.load( img_dir + img_name ) for img_name in img_names ], dtype = float)
     stack_descriptor_dict['img_paths'] = [ img_dir + img_name for img_name in img_names ]
+    stack = np.array([ np.load( full_img_name ) for full_img_name in stack_descriptor_dict['img_paths'] ], dtype = float)
     
     #to do, some descriptor of the images for provenance: commit and input params for base shape gen
     #stack_descriptor_dict['base_shape_gen_inputs'] = [ img_dir + img_name for img_name in img_names ]
@@ -155,7 +163,7 @@ def load_npy_img_dirs_into_stack( img_dir ):
 
 
 import matplotlib.pyplot as plt
-img_dir = '/Users/deanpospisil/Desktop/net_code/images/baseimgs/formlet/'  
+img_dir = '/Users/deanpospisil/Desktop/net_code/images/baseimgs/PC370/'  
 
 
 stim_trans_dict = stim_idprestrans_generator(shape = [1,2], scale = (1,1,1), x = (-20,20,4), y = None, rotation = None)
@@ -164,7 +172,7 @@ stack, stack_desc = load_npy_img_dirs_into_stack( img_dir )
 
 trans_stack = imp.imgStackTransform( stim_trans_dict, stack )
 
-img_num = 7
+img_num = 2
 
 plt.subplot(1,3,1)
 plt.imshow( stack[stim_trans_dict['shape'][img_num]], interpolation = 'none', cmap = plt.cm.Greys_r  )
@@ -172,7 +180,7 @@ plt.subplot(1,3,2)
 plt.imshow( trans_stack[img_num], interpolation = 'none', cmap = plt.cm.Greys_r  )
 
 plt.subplot(1,3,3)
-plt.plot(trans_stack[img_num, :, 100])
+plt.plot(trans_stack[img_num, :, 120])
 print( str(stim_trans_dict['shape'][img_num]) + ' scale ' + str(stim_trans_dict['scale'][img_num]) + ' x ' + str(stim_trans_dict['x'][img_num]) )
 
 ANNDir = '/home/dean/caffe/models/bvlc_reference_caffenet/'

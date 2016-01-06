4ef469491ee210efef25a682c3ce41cd22c843b2
dean <deanp3@uw.edu>
2016-01-06 12:29:30 -0800

    a

--- mess_with_git_python.py
+++ mess_with_git_python.py
@@ -7,7 +7,7 @@ Created on Tue Jan  5 10:29:45 2016
 
 import os
 from git import Repo
-
+from datetime import datetime
 # rorepo is a a Repo instance pointing to the git-python repository.
 # For all you know, the first argument to Repo is a path to the repository
 # you want to work with
@@ -16,9 +16,11 @@ assert not repo.bare
 
 git = repo.git
 #parse this for status
-
+time_str = str(datetime.now())
 #repo.git.add( 'somefile' )
+commit_message = "git commit -a -m  %r." %time_str
 
+s = commit_message
 os.system('git commit -a -m "a"')
 modified = '\n\n\tmodified:'
 status = git.status()

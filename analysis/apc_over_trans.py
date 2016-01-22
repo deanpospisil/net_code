5cf8929710068894b6103dc574a358fc87c3fa41
Dean Pospisil <dpospisi@students.pitzer.edu>
2016-01-22 10:44:44 -0800

    TI pc

--- analysis/apc_over_trans.py
+++ analysis/apc_over_trans.py
@@ -8,7 +8,7 @@ import numpy as np
 import xray as xr
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 import os, sys
 
 abspath = os.path.abspath(__file__)
@@ -19,27 +19,26 @@ sys.path.append( cwd)
 
 #best fit apc
 # effect of blur
-dm = xr.open_dataset(cwd +'/responses/apc_models.nc', chunks={'models': 100} )
-da = xr.open_dataset(cwd +'/responses/PC370_shapes_0.0_369.0_370_x_-100.0_100.0_201.nc', chunks={'x': 1, 'unit':100} )
+dm = xr.open_dataset(cwd +'/responses/apc_models.nc', chunks = {'models' : 7}  )
+da = xr.open_dataset(cwd +'/responses/PC370_shapes_0.0_369.0_370_x_-100.0_100.0_201.nc', chunks={ 'unit': 7} )
 
-unitsel = np.arange(0, da.dims['unit'], 10 )
+unitsel = np.arange(0, da.dims['unit'], 1 )
 da = da.sel(unit = unitsel, method = 'nearest' )
-
-unitsel = np.arange(0, da.dims['unit'], 100 )
-da = da.sel(x = [4, 2, 0 ,2 ,4 ], method = 'nearest' )
-
-da = da.chunk(chunks={'x': 1, 'unit':100})
-
 
 da_n = da - da.mean('shapes').mean('x')
 da_n = da_n / ( ( da_n**2 ).sum('shapes').sum('x') )**0.5
 
-dm = dm / da_n.dims['x']**0.5 #need to adjust variance, performing correlation over translations
 
+dm_s = (da_n*dm).sum('shapes')*dm #get the projection of the apc vectors on each translation
 
-fitm = (da_n*dm).sum('shapes').sum('x').max('models')
+#turn the vector of a scaled apc model over translations into a unit vector
+dm_s = dm_s / ( ( dm_s**2 ).sum('shapes').sum('x') )**0.5 
 
-
+#get the correlation of each of these scaled apc models
+fitm = (da_n*dm_s).sum('shapes').sum('x')
+fitm = fitm.chunk({'unit':100})
+fitm = fitm.max('models')
+print('chunked')
 fitm.to_netcdf(cwd +'/responses/apc_models_r_trans.nc')
 fitm = xr.open_dataset(cwd +'/responses/apc_models_r_trans.nc' )
 

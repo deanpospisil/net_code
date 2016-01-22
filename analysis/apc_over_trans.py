ea544315341843184cde61ccbfa84063adb21ce8
Dean Pospisil <dean@qufu.local>
2016-01-22 11:55:26 -0800

    getting ready for merge

--- analysis/apc_over_trans.py
+++ analysis/apc_over_trans.py
@@ -22,11 +22,10 @@ sys.path.append( cwd)
 dm = xr.open_dataset(cwd +'/responses/apc_models.nc', chunks={'models': 100} )
 da = xr.open_dataset(cwd +'/responses/PC370_shapes_0.0_369.0_370_x_-100.0_100.0_201.nc', chunks={'x': 1, 'unit':100} )
 
-unitsel = np.arange(0, da.dims['unit'], 10 )
+unitsel = np.arange(0, da.dims['unit'], 1000 )
 da = da.sel(unit = unitsel, method = 'nearest' )
 
-unitsel = np.arange(0, da.dims['unit'], 100 )
-da = da.sel(x = [4, 2, 0 ,2 ,4 ], method = 'nearest' )
+da = da.sel(x = [2, 0   ], method = 'nearest' )
 
 da = da.chunk(chunks={'x': 1, 'unit':100})
 
@@ -34,17 +33,20 @@ da = da.chunk(chunks={'x': 1, 'unit':100})
 da_n = da - da.mean('shapes').mean('x')
 da_n = da_n / ( ( da_n**2 ).sum('shapes').sum('x') )**0.5
 
-dm = dm / da_n.dims['x']**0.5 #need to adjust variance, performing correlation over translations
+#dm = dm / da_n.dims['x']**0.5 #need to adjust variance, performing correlation over translations
 
+dm_s = (da_n*dm).sum('shapes')*dm
 
-fitm = (da_n*dm).sum('shapes').sum('x').max('models')
+dm_s = dm_s / ( ( dm_s**2 ).sum('shapes').sum('x') )**0.5
 
+fitm = (da_n*dm_s).sum('shapes').sum('x').max('models')
 
+#
 fitm.to_netcdf(cwd +'/responses/apc_models_r_trans.nc')
-fitm = xr.open_dataset(cwd +'/responses/apc_models_r_trans.nc' )
-
-b = fitm.to_dataframe()
-b.set_index(['layer_unit' ,'layer'], append=True, inplace=True)
-plt.close('all')
-
-sns.boxplot(x="layer_label", y="resp" , data=b[b['resp']>0], whis=1)
+#fitm = xr.open_dataset(cwd +'/responses/apc_models_r_trans.nc' )
+#
+#b = fitm.to_dataframe()
+#b.set_index(['layer_unit' ,'layer'], append=True, inplace=True)
+#plt.close('all')
+#
+#sns.boxplot(x="layer_label", y="resp" , data=b[b['resp']>0], whis=1)

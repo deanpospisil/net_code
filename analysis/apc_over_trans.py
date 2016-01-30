bf2dbad10f73782cc87a3eb1e77cc15568932efd
Dean Pospisil <dean@qufu.local>
2016-01-29 17:52:04 -0800

    big run!

--- analysis/apc_over_trans.py
+++ analysis/apc_over_trans.py
@@ -21,37 +21,36 @@ sys.path.append( cwd + '/xarray')
 import xarray as xr
 
 
-#best fit apc
-# effect of blur
-dm = xr.open_dataset(cwd +'/responses/apc_models.nc',chunks = {'models': 1000, 'shapes': 370}  )
-da = xr.open_dataset(cwd +'/responses/PC370_shapes_0.0_369.0_370_x_-100.0_100.0_201.nc',chunks = {'unit': 100, 'x': 100} )
 
+dm = xr.open_dataset(cwd +'/responses/apc_models.nc',chunks = {'models': 1000, 'shapes': 370}  )
+da = xr.open_dataset( cwd +'/responses/PC370_shapes_0.0_369.0_370_x_-100.0_100.0_201.nc',chunks = {'unit': 100, 'x': 100} )
 #dm = xr.open_dataset(cwd +'/responses/apc_models.nc'  )
 #da = xr.open_dataset(cwd +'/responses/PC370_shapes_0.0_369.0_370_x_-100.0_100.0_201.nc')
+#dm = dm.load()
+#da = da.load()
 
+dm = dm.sel(models = range(100), method = 'nearest' )
 
-#dm = dm.sel(models = range(100), method = 'nearest' )
-
-unitsel = np.arange(0, da.dims['unit'], 1)
+unitsel = np.arange(0, da.dims['unit'], 1000)
 da = da.sel(unit = unitsel, method = 'nearest' )
 
 
 xsel = np.arange(-da.dims['x'] / 10., da.dims['x'] / 10., 2 )
 da = da.sel(x = [8, 6, 4, 2, 0, 2, 4, 6, 8], method = 'nearest' )
 
-#dm = dm.sel(models = range(100))
+dm = dm.sel(models = range(100))
 
-t = da['resp'].dot(dm['resp'])
+t = da['resp'].dot( dm['resp'] )
 #t is the projection of each apc model onto each of the translated responses
 
-n = da.reduce(np.linalg.norm, ['shapes', 'x'] )
-t = t.reduce(np.linalg.norm,  'x' )
+n = da['resp'].vnorm(['shapes', 'x'] )
+t = t.vnorm( 'x' )
 
 cor = (t/n).max('models')
 
 
 
-#d.to_hdf5(cwd +'/responses/apc_models_r_trans.nc', {'/t': t})
+cor.to_hdf5(cwd +'/responses/apc_models_r_trans.nc', {'/t': t})
 #da_n = da - da.mean('shapes').mean('x')
 #da_n = da_n / ( ( da_n**2 ).sum('shapes').sum('x') )**0.5
 #
@@ -76,3 +75,4 @@ cor = (t/n).max('models')
 #
 #
 #sns.boxplot(x="layer_label", y="resp" , data=b)
+dm.std

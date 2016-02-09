b5461f64149e6df3e38f6e297de136d7740d0d97
dean <deanp3@uw.edu>
2016-02-09 15:53:40 -0800

    correlation across multiple dims

--- analysis/apc_over_trans.py
+++ analysis/apc_over_trans.py
@@ -23,37 +23,52 @@ import xarray as xr
 
 
 dm = xr.open_dataset(cwd +'/responses/apc_models.nc',chunks = {'models': 1000, 'shapes': 370}  )
-da = xr.open_dataset( cwd +'/responses/PC370_shapes_0.0_369.0_370_x_-100.0_100.0_201.nc',chunks = {'unit': 100, 'x': 100} )
-#da = xr.open_dataset( cwd +'/responses/PC370_shapes_0.0_369.0_370.nc',chunks = {'unit': 100}  )
+#da = xr.open_dataset( cwd +'/responses/PC370_shapes_0.0_369.0_370_x_-100.0_100.0_201.nc',chunks = {'unit': 100, 'x': 100} )
+da = xr.open_dataset( cwd +'/responses/PC370_shapes_0.0_369.0_370_x_-120.0_120.0_11_y_-120.0_120.0_11.nc',chunks = {'unit': 100}  )
 #da = xr.open_dataset( cwd +'/responses/PC370_shapes_matlab.nc',chunks = {'unit': 100}  )
+
 
 #dm = dm.sel(models = range(100), method = 'nearest' )
 #unitsel = np.arange(0, da.dims['unit'], 1000)
 #da = da.sel(unit = unitsel, method = 'nearest' )
 ##xsel = np.arange(-da.dims['x'] / 10., da.dims['x'] / 10., 2 )
 da = da.sel(x = [ 0, 10], method = 'nearest' )
+da = da.sel(y = [ 0], method = 'nearest' )
 #dm = dm.sel(models = range(100))
 
 #using xray
-da['resp'] = da['resp'] - da['resp'].mean(('shapes','x'))
+da = da['resp'] - da['resp'].mean(('shapes'))
+dm = dm['resp']
 
-n =  da['resp'].vnorm(('shapes'))
-n =  n.vnorm(('x'))
-da['resp'] = da['resp']/n
+n =  da.vnorm(('shapes'))
+resp_norm =  n.vnorm(('x'))
 
-t = da['resp'].dot( dm['resp'] )
-t = t*dm['resp']
-t = t - t.mean(('shapes','x'))
+proj_resp_on_model = da.dot(dm)
 
-n =  t.vnorm(('shapes'))
-n =  n.vnorm(('x'))
-t = t / n
+proj_resp_on_model_norm =  proj_resp_on_model.vnorm(('shapes')).vnorm(('x'))
 
-ct = da['resp'].dot( t )
+all_cor = proj_resp_on_model_norm / resp_norm
 
-ct = ct.sum('x')
+cor = all_cor.max('models')
 
-cor = (t).max('models')
+
+cor.to_dataset('cor').to_netcdf(cwd +'/responses/apc_models_r_repro.nc')
+
+
+
+fitm = xr.open_dataset(cwd +'/responses/apc_models_r_repro.nc' )
+b = fitm.to_dataframe()
+b.set_index(['layer_unit' ,'layer'], append=True, inplace=True)
+
+
+#sns.boxplot(x="layer_label", y="cor" , data=b)
+per = b[b>0.5].groupby('layer_label').count()/b.groupby('layer_label').count()
+per.plot(kind = 'bar')
+plt.ylim((0,1))
+plt.title('Percent units > 0.5 Correlation a')
+
+
+
 
 ##using numpy 
 #nUnits = resp.shape[1]
@@ -69,18 +84,4 @@ cor = (t).max('models')
 #rmx = xr.DataArray(rm , coords= cor.coords, dims = cor.dims)
 #
 #
-#rmx.to_dataset('cor').to_netcdf(cwd +'/responses/apc_models_r_repro_np.nc')
-cor.to_dataset('cor').to_netcdf(cwd +'/responses/apc_models_r_repro.nc')
-
-
-
-fitm = xr.open_dataset(cwd +'/responses/apc_models_r_repro.nc' )
-b = fitm.to_dataframe()
-b.set_index(['layer_unit' ,'layer'], append=True, inplace=True)
-
-
-#sns.boxplot(x="layer_label", y="cor" , data=b)
-per = b[b>0.5].groupby('layer_label').count()/b.groupby('layer_label').count()
-per.plot(kind = 'bar')
-plt.ylim((0,1))
-plt.title('Percent units > 0.5 Correlation a')
+#rmx.to_dataset('cor').to_netcdf(cwd +'/responses/apc_models_r_repro_np.nc')Z

b2e50ec375fe33c563ee07356afbf83a3cd4c5ba
dean <deanp3@uw.edu>
2016-02-13 10:20:44 -0800

    getting up to date

--- analysis/apc_over_trans.py
+++ analysis/apc_over_trans.py
@@ -22,17 +22,17 @@ import xarray as xr
 
 
 
-#dm = xr.open_dataset(cwd +'/responses/apc_models.nc',chunks = {'models': 1000, 'shapes': 370}  )
+dm = xr.open_dataset(cwd +'/responses/apc_models.nc',chunks = {'models': 1000, 'shapes': 370}  )
 #da = xr.open_dataset( cwd +'/responses/PC370_shapes_0.0_369.0_370_x_-100.0_100.0_201.nc',chunks = {'unit': 100, 'x': 100} )
 #da = xr.open_dataset( cwd +'/responses/PC370_shapes_0.0_369.0_370.nc',chunks = {'unit': 100}  )
-#da = xr.open_dataset( cwd +'/responses/PC370_shapes_matlab.nc',chunks = {'unit': 100}  )
-da = xr.open_dataset( cwd +'/responses/PC370_shapes_0.0_369.0_370_x_-50.0_50.0_101.nc',chunks = {'unit': 100}  )
+da = xr.open_dataset( cwd +'/responses/PC370_shapes_matlab.nc',chunks = {'unit': 100}  )
+
 
 #dm = dm.sel(models = range(100), method = 'nearest' )
 #unitsel = np.arange(0, da.dims['unit'], 1000)
 #da = da.sel(unit = unitsel, method = 'nearest' )
 ##xsel = np.arange(-da.dims['x'] / 10., da.dims['x'] / 10., 2 )
-da = da.sel(x = [ 0 ], method = 'nearest' )
+#da = da.sel(x = [ 0 ], method = 'nearest' )
 
 #dm = dm.sel(models = range(100))
 
@@ -59,22 +59,18 @@ cor = all_cor.max('models')
 
 cor.to_dataset('cor').to_netcdf( cwd + '/responses/apc_models_r_repro.nc')
 
-fitm = xr.open_dataset(cwd +'/responses/apc_models_r_trans1.nc.nc' )
-
-fitm = xr.open_dataset(cwd +'/responses/apc_models_r_trans1.nc' )
-
-
+fitm = xr.open_dataset(cwd +'/responses/apc_models_r_repro.nc' )
 b = fitm.to_dataframe()
 b.set_index(['layer_unit', 'layer'], append=True, inplace=True)
 
 
-sns.boxplot(x="layer_label", y="cor" , data=b)
-fillb = b.fillna(0)
+#sns.boxplot(x="layer_label", y="cor" , data=b)
+b = b.fillna(0)
 
-per = b[b>0.5].groupby('layer_label').count()/fillb.groupby('layer_label').count()
+per = b[b>0.5].groupby('layer_label').count()/b.groupby('layer_label').count()
 per.plot(kind = 'bar')
 plt.ylim((0,1))
-plt.title('Percent units > 0.5 Correlation Across Translations')
+plt.title('Percent units > 0.5 Correlation a')
 
 
 

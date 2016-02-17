261dc872ea5d431bf5bfe7207017937f75c17627
Dean Pospisil <dean@qufu.local>
2016-02-16 17:14:28 -0800

    a

--- nets/across_nets.py
+++ nets/across_nets.py
@@ -15,14 +15,14 @@ baseImageList = ['PC370', 'formlet']
 base_image_nm = baseImageList[0]
 
 ann_fn = 'caffenet_train_iter_'
-#get stimuli list
+
+
 stim_trans_cart_dict, stim_trans_dict = cf.stim_trans_generator(shapes=range(370),
                                                              blur=None,
                                                              scale =None,
                                                              x=(-50, 50, 101),
                                                              y=None,
                                                              rotation = None)
-
 '''
 for train_iter in range(10000,990000,50000):
 
@@ -30,24 +30,19 @@ for train_iter in range(10000,990000,50000):
                  require_provenance=True)
     ds = da.to_dataset(name = 'resp')
     ds.to_netcdf(top_dir + 'analysis/data/iter_' + str(train_iter) + '.nc')
-'''
 
+'''
 resp_name = cf.get_net_resp_name(stim_trans_dict, ann_fn, base_image_nm)
 iter_ind = range(10000, 990000, 50000)
-iter_ind = [10000, 110000, 160000]
+#iter_ind = [10000, 110000, 160000]
 
 foo = []
 for i, train_iter in enumerate(iter_ind):
     fn = top_dir + 'analysis/data/iter_' + str(train_iter) + '.nc'
-    foo.append(xr.open_dataset(fn, chunks = {'unit':100, 'shapes': 370})['resp'])
+    foo.append(xr.open_dataset(fn, chunks = {'unit':100, 'shapes': 370}))
 
-indArr = xr.DataArray(iter_ind, name='train_iter')
-over_iter = xr.concat(foo, indArr)
-ds = over_iter.to_dataset(name = 'resp')
+ds = xr.auto_combine(foo, concat_dim = 'iter')
 ds.to_netcdf(top_dir + 'analysis/data/'+ resp_name )
-
-
-
 
 
 

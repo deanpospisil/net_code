aa305d984dfef38891be7f1b3bcd5a8b17299b53
Dean Pospisil <dpospisi@students.pitzer.edu>
2016-02-19 17:50:25 -0800

    provenance commit 2016-02-19 17:50:24.334742.

--- analysis/code/apc_cor_nd.py
+++ analysis/code/apc_cor_nd.py
@@ -53,16 +53,16 @@ dmod = xr.open_dataset(top_dir + 'analysis/data/models/apc_models.nc',chunks = {
 #dmod = dmod.sel(models = range(10), method = 'nearest' )
 ds = xr.open_mfdataset(top_dir + 'analysis/data/iter_*.nc', concat_dim = 'niter', chunks = {'unit':100, 'shapes': 370})
 da = ds.to_array().chunk(chunks = {'niter':1, 'unit':100, 'shapes': 370})
-da = da.sel(x = np.linspace(-50, 50, 25), method = 'nearest' )
-da = da.sel(niter = np.linspace(0, da.coords['niter'].shape[0], 10),  method = 'nearest')
+da = da.sel(x = np.linspace(-50, 50, 51), method = 'nearest' )
+da = da.sel(niter = np.linspace(0, da.coords['niter'].shape[0], 20),  method = 'nearest')
 #da = da.sel(unit = range(10),  method = 'nearest')
 
 
 for iterind in da.niter.values:
-    da_c = da.sel(niter=[iterind])
+    da_c = da.sel(niter=iterind)
     cor = cor_resp_to_model(da_c, dmod, fit_over_dims = ('x',))
     cor.to_dataset(name='r').to_netcdf(top_dir + 'analysis/data/r_iter_' + str(iterind) + '.nc')
 
-ds = xr.open_mfdataset(top_dir + 'analysis/data/r_iter_*', concat_dim = 'niter')
+ds = xr.open_mfdataset(top_dir + 'analysis/data/r_iter_*.nc', concat_dim = 'niter')
 ds.to_netcdf(top_dir + 'analysis/data/r_iter_total_' + str(da.niter.shape[0]) +  '.nc')
 

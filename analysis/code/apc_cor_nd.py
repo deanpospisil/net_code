e09443221381371c653c889e01c1f20b4f3571d0
Dean Pospisil <dpospisi@students.pitzer.edu>
2016-02-24 13:21:07 -0800

    async prob

--- analysis/code/apc_cor_nd.py
+++ analysis/code/apc_cor_nd.py
@@ -50,15 +50,15 @@ def cor_resp_to_model(da, dmod, fit_over_dims = None):
 
 dmod = xr.open_dataset(top_dir + 'analysis/data/models/apc_models.nc',chunks = {'models': 1000, 'shapes': 370}  )
 #da = xr.open_dataset( top_dir + 'analysis/data/PC370_shapes_0.0_369.0_370_x_-50.0_50.0_101.nc', chunks = {'unit': 100}  )
-#dmod = dmod.sel(models = range(10), method = 'nearest' )
+dmod = dmod.sel(models = range(10), method = 'nearest' )
 ds = xr.open_mfdataset(top_dir + 'analysis/data/iter_*.nc', concat_dim = 'niter', chunks = {'unit':100, 'shapes': 370})
-da = ds.to_array().chunk(chunks = {'niter':1, 'unit':100, 'shapes': 370})
+da = ds['resp'].chunk(chunks = {'niter':1, 'unit':100, 'shapes':370})
 da = da.sel(x = np.linspace(-50, 50, 51), method = 'nearest' )
-da = da.sel(niter = np.linspace(0, da.coords['niter'].shape[0], 20),  method = 'nearest')
+#da = da.sel(niters=np.linspace(0, da.coords['niter'].shape[0], 19),  method = 'nearest')
 #da = da.sel(unit = range(10),  method = 'nearest')
 
 
-for iterind in da.niter.values:
+for iterind in ds.niter.values:
     da_c = da.sel(niter=iterind)
     cor = cor_resp_to_model(da_c, dmod, fit_over_dims = ('x',))
     cor.to_dataset(name='r').to_netcdf(top_dir + 'analysis/data/r_iter_' + str(iterind) + '.nc')

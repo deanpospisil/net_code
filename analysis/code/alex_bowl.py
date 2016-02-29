286995005bf96605ef39c0d3f565267f7ee86194
dean <deanp3@uw.edu>
2016-02-28 22:41:39 -0800

    corr for trans and alexbowl working

--- analysis/code/alex_bowl.py
+++ analysis/code/alex_bowl.py
@@ -6,40 +6,51 @@ Created on Fri Feb 26 17:12:56 2016
 """
 
 import scipy.io as l
-import os
-import pickle
+import os, sys
 import numpy as np
-import sys
-top_dir = os.getcwd().split('net_code')[0] + '/net_code/'
-sys.path.append(top_dir)
-sys.path.append( top_dir + '/xarray')
+import pickle
+
+top_dir = os.getcwd().split('net_code')[0] 
+sys.path.append(top_dir + 'net_code/common/')
+sys.path.append( top_dir + 'xarray/')
+
 import xarray as xr
+import apc_model_fit as ac
 
-
-m = l.loadmat(top_dir + 'analysis/data/pyresponses.mat')
+#put responses into xarray
+m = l.loadmat(top_dir + '/net_code/data/responses/pyresponses.mat')
 m = m['pyresponses']
 
 shape_id = m[:, 0]
 resp = m[:, 1:]
-
 da = xr.DataArray(resp, dims=['shapes', 'unit'], coords= [shape_id, range(resp.shape[1])])
 
-ds = da.to_dataset(name = 'resp')
-ds.to_netcdf(top_dir +'analysis/data/alex_bowl.nc' )
+ds = da.to_dataset(name='resp')
+ds.to_netcdf(top_dir + 'net_code/data/responses/alex_bowl.nc' )
 
-import apc_cor_nd as ac
+#open those responses, and build apc models for their shapes
+with open(top_dir + 'net_code/data/models/PC370_params.p', 'rb') as f:
+    shape_dict_list = pickle.load(f)
 
-dmod = xr.open_dataset(top_dir + 'analysis/data/models/apc_models_bowl.nc',
+da = xr.open_dataset(top_dir + 'net_code/data/responses/alex_bowl.nc', chunks = {'shapes':370})['resp']
+
+shape_id = da.coords['shapes'].values
+
+shape_dict_list = [shape_dict_list[sn] for sn in shape_id.astype(int)]
+
+maxAngSD = np.deg2rad(171)
+minAngSD = np.deg2rad(23)
+maxCurSD = 0.98
+minCurSD = 0.09
+nMeans = 16
+nSD = 16
+fn = 'apc_models_bowl.nc'
+dam = ac.make_apc_models(shape_dict_list, shape_id, fn, nMeans, nSD, maxAngSD, minAngSD,
+                      maxCurSD, minCurSD, prov_commit=False)
+
+#load the models you made, and fit them to the cells responses
+dmod = xr.open_dataset(top_dir + 'net_code/data/models/apc_models_bowl.nc',
                        chunks = {'models': 1000, 'shapes': 370}  )['resp']
-da = xr.open_dataset(top_dir + 'analysis/data/alex_bowl.nc', chunks = {'shapes': 370})['resp']
-cor = ac.cor_resp_to_model(da, dmod)
-
-bf = dmod.sel(models=cor[0].coords['models']).load()
-r = da[:, 0].load()
-
-import matplotlib.pyplot as plt
-plt.plot(bf)
-plt.plot((r-np.mean(r))/30.)
-
-
-np.corrcoef([bf, r-np.mean(r)])
+da = xr.open_dataset(top_dir + 'net_code/data/responses/alex_bowl.nc', 
+                     chunks = {'shapes': 370})['resp']
+cor = ac.cor_resp_to_model(da, dmod, fit_over_dims=None, prov_commit=False)
--- common/apc_model_fit.py
+++ common/apc_model_fit.py
@@ -4,27 +4,22 @@ Created on Wed Jan  6 18:28:57 2016
 
 @author: dean
 """
-
 # analysis
 import scipy.stats as st
 import numpy as np
 import warnings
-import os
-import sys
+import os, sys
 from collections import OrderedDict as ord_d
-import matplotlib.pyplot as plt
 
-top_dir = os.getcwd().split('net_code')[0] + 'net_code/'
-sys.path.append(top_dir)
-
-
+top_dir = os.getcwd().split('net_code')[0] 
+sys.path.append(top_dir + 'net_code/')
 sys.path.append(top_dir + 'xarray')
 
 import xarray as xr
 import d_misc as dm
-import pickle
 
-def get_2d_dims_right(vec, dims_order=(1,0)):
+
+def get_2d_dims_right(vec, dims_order=(1, 0)):
     dims = vec.shape
     if len(dims)>2:
         warnings.warn('model params should not have more than 2-d')
@@ -41,12 +36,10 @@ def get_2d_dims_right(vec, dims_order=(1,0)):
 #takes a set of points in apc plane and makes prediction based on different receptive fields
 def apc_models( shape_dict_list = [{'curvature': None, 'orientation': None} ],
                                    model_params_dict={'or_sd': [3.14], 'or_mean':[3.14], 'cur_mean':[1], 'cur_sd':[0.1]}):
-
      # make sure everything has the right dimensionality for broadcating
     for key in model_params_dict:
-        vec = np.array( model_params_dict[key] )
-        model_params_dict[key] = get_2d_dims_right(vec, dims_order=(1,0) )
-
+        vec = np.array(model_params_dict[key])
+        model_params_dict[key] = get_2d_dims_right(vec, dims_order=(1,0))
 
     # make sure everything has the right dimensionality for broadcating, figure out a more succint way to do this
     for ind, a_shape in enumerate(shape_dict_list):
@@ -61,7 +54,7 @@ def apc_models( shape_dict_list = [{'curvature': None, 'orientation': None} ],
     norm_rv = st.norm( scale = model_params_dict['cur_sd'] , loc = model_params_dict['cur_mean'] )
 
     #get responses to all points for each axis ap and c then their product, then the max of all those points as the resp
-    model_resp_all_apc_points = [ von_rv.pdf( apc_points['orientation'] ) * norm_rv.pdf( apc_points['curvature'] ) for apc_points in shape_dict_list ]
+    model_resp_all_apc_points = [von_rv.pdf( apc_points['orientation']) * norm_rv.pdf( apc_points['curvature'] ) for apc_points in shape_dict_list]
     model_resp = np.array([ np.max( a_shape, axis = 0 ) for a_shape in model_resp_all_apc_points])
 
     #mean subtract
@@ -75,6 +68,8 @@ def apc_models( shape_dict_list = [{'curvature': None, 'orientation': None} ],
 def make_apc_models(shape_dict_list, shape_id, fn, nMeans, nSD, maxAngSD, minAngSD, maxCurSD, minCurSD,
                     prov_commit = False):
     #make this into a pyramid based on d-prime
+    fn = top_dir + 'net_code/data/models/' + fn
+                    
     orMeans = np.linspace(0, 2*np.pi - 2*np.pi / nMeans, nMeans)
     orSDs = np.logspace(np.log10( minAngSD ), np.log10( maxAngSD ), nSD )
     curvMeans = np.linspace( -0.5, 1, nMeans )
@@ -86,42 +81,58 @@ def make_apc_models(shape_dict_list, shape_id, fn, nMeans, nSD, maxAngSD, minAng
 
     model_params_dict = dm.cartesian_prod_dicts_lists( model_params_dict )
 
+    if not os.path.isfile(fn):
+        model_resp = apc_models(shape_dict_list=shape_dict_list,
+                                model_params_dict=model_params_dict)
+        dam = xr.DataArray(model_resp, dims = ['shapes', 'models'], coords=[shape_id, range(model_resp.shape[1])])
+    
+        for key in model_params_dict.keys():
+            dam[key] = ('models', np.squeeze(model_params_dict[key]))
+    
+        if prov_commit:
+            sha = dm.provenance_commit(top_dir)
+            dam.attrs['model'] = sha    
+        ds = xr.Dataset({'resp': dam})
+        ds.to_netcdf(fn)
+        return ds
+    else:
+        warnings.warn('Model File has Already Been Written.')
+        return xr.open_dataset(fn)
+          
+def cor_resp_to_model(da, dmod, fit_over_dims=None, prov_commit=False):
+    #typically takes da, data, and dm, a set of linear models, an fn to write to,
+    #and finally fit_over_dims which says over what dims is a models fit supposed to hold.
+    da = da - da.mean(('shapes'))
+    resp_n = da.vnorm(('shapes'))
+    proj_resp_on_model = da.dot(dmod)
 
-    model_resp = apc_models(shape_dict_list=shape_dict_list,
-                            model_params_dict=model_params_dict)
+    if not fit_over_dims == None:
+        resp_norm = resp_n.vnorm(fit_over_dims)
+        proj_resp_on_model_var = proj_resp_on_model.sum(fit_over_dims)
+        n_over = 0
+        #count up how many unit vectors you'll be applying for each r.
+        for dim in fit_over_dims:
+            n_over = n_over + len(da.coords[dim].values)
+    else:
+        resp_norm = resp_n
+        proj_resp_on_model_var = proj_resp_on_model
+        n_over = 1
 
-    dam = xr.DataArray(model_resp, dims = ['shapes', 'models'], coords=[shape_id, range(model_resp.shape[1])])
-
-    for key in model_params_dict.keys():
-        dam[key] = ('models', np.squeeze(model_params_dict[key]))
-
-    if prov_commit:
+    all_cor = (proj_resp_on_model_var) / (resp_norm * (n_over**0.5))
+    all_cor = all_cor.load()
+    all_cor = all_cor.dropna('unit')
+    
+    corarg = all_cor.argmax('models')
+    model_fit_params = dmod.coords['models'][corarg]    
+    cor = all_cor.max('models')
+        
+    for key in model_fit_params.coords.keys():
+        cor[key] = ('unit', np.squeeze(model_fit_params[key]))
+    
+    if prov_commit==True and hasattr(dmod.attrs, 'model'):
         sha = dm.provenance_commit(top_dir)
-        dam.attrs['model'] = sha
-
-
-    ds = xr.Dataset({'resp': dam})
-    ds.to_netcdf(top_dir + 'analysis/data/models/' + fn)
-    return dam
-
-with open(top_dir + 'analysis/data/models/PC370_params.p', 'rb') as f:
-    shape_dict_list = pickle.load(f)
-
-da = xr.open_dataset(top_dir + 'analysis/data/alex_bowl.nc', chunks = {'shapes':370})['resp']
-
-shape_id = da.coords['shapes'].values
-
-shape_dict_list = [shape_dict_list[sn] for sn in shape_id.astype(int)]
-
-maxAngSD = np.deg2rad(171)
-minAngSD = np.deg2rad(23)
-maxCurSD = 0.98
-minCurSD = 0.09
-
-nMeans = 16
-nSD = 16
-fn = 'apc_models_bowl.nc'
-dam = make_apc_models(shape_dict_list, shape_id, fn, nMeans, nSD, maxAngSD, minAngSD,
-                      maxCurSD, minCurSD, prov_commit=False)
+        cor.attrs['analysis'] = sha
+        cor.attrs['model'] = dmod.attrs['model']
+    return cor
 
 

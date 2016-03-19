6a43d29b14a82cb1f5666ef93dc7d8ff5a5173f3
Dean Pospisil <dean@qufu.local>
2016-03-18 18:31:13 -0700

    rot cor working!

--- misc/figure_out_circular_sampling.py
+++ misc/figure_out_circular_sampling.py
@@ -29,126 +29,114 @@ def get2dCfIndex(xsamps,ysamps,fs):
     return c
 
 def cart_to_polar_2d_fft(im, sample_rate_mult):
-    
+
     #get polar resampling coordinates
     n_pix = np.size(im,0)/2.
     npts_mag = int(np.size(im,0) / 2.)
     npts_angle = int(np.pi * 2 * n_pix)
     angles_vec = np.linspace(0, 2*np.pi, npts_angle)
     magnitudes = np.linspace(0, 0.5, npts_mag)
-    angles, magnitudes = np.meshgrid(angles_vec, magnitudes) 
+    angles, magnitudes = np.meshgrid(angles_vec, magnitudes)
     x = np.expand_dims((magnitudes * np.cos(angles) + 0.5).ravel(), 1)
     y = np.expand_dims((magnitudes * np.sin(angles) + 0.5).ravel(), 1)
-    
-    #get fourier coefficients of image    
+
+    #get fourier coefficients of image
     ftl = np.fft.rfft2(im)
     amp = abs(ftl) / np.size(ftl)#scale amplitude so these terms are coefficients
     phase = np.exp(1j * np.angle(ftl))#get phase of these coefficients
     coef = (amp * phase).ravel()
-    
-    #get fourier basis    
+
+    #get fourier basis
     _ = get2dCfIndex(np.size(im,0), np.size(im,1), np.size(im,0))
     w1 = np.real(_)[ :np.size(ftl,0), :np.size(ftl,1) ].ravel()
     w2 = np.imag(_)[ :np.size(ftl,0), :np.size(ftl,1) ].ravel()
     basis = np.exp(1j * 2*np.pi * (x * w1 + y * w2))
-    
+
     #apply coef to basis, and sum
     circ_vals = np.sum(coef * basis, 1, dtype=np.complex128)
     circ_vals.shape = (npts_mag, npts_angle)
     #circ_vals = circ_vals * magnitudes*2*np.pi
-    
+
     return np.real(circ_vals), angles_vec
-    
+
 def cart_to_polar_2d_lin(im, sample_rate_mult):
-    
+
     x = np.arange(im.shape[1])
     y = np.arange(im.shape[0])
-    f = interpolate.interp2d(x, y, im, kind='linear')    
-    
+    f = interpolate.interp2d(x, y, im, kind='linear')
+
     #get polar resampling coordinates
     n_pix = np.size(im,0)/2.
     npts_mag = int(np.size(im, 0) / 2.)*sample_rate_mult
     npts_angle = int(np.pi * 2 * n_pix)*sample_rate_mult
-    
+
     angles_vec = np.linspace(0, 2*np.pi, npts_angle)
     magnitudes_vec = np.linspace(0, n_pix-1, npts_mag)
-    angles, magnitudes = np.meshgrid(angles_vec, magnitudes_vec) 
+    angles, magnitudes = np.meshgrid(angles_vec, magnitudes_vec)
     xnew = (magnitudes * np.cos(angles)+n_pix).ravel()
     ynew = (magnitudes * np.sin(angles)+n_pix).ravel()
     f = interpolate.RegularGridInterpolator((x, y), im, method='linear')
-    
+
     pts = np.fliplr(np.array([xnew, ynew]).T)
     im_pol = f(pts).reshape(npts_mag, npts_angle)
     im_pol = im_pol * magnitudes*2*np.pi
-    
+
     return im_pol, angles_vec
 
-    
-os.chdir('/Users/deanpospisil/Desktop/')
-im=mpimg.imread('soft_on_orig.png')
-im = np.sum(im, 2) 
+def circ_cor(im1, im2, sample_rate_mult=2):
+    _, angles = cart_to_polar_2d_lin(im1, sample_rate_mult)
+    circ_vals1 = _/np.linalg.norm(_)
 
-sample_rate_mult = 2
+    _, angles = cart_to_polar_2d_lin(im2, sample_rate_mult)
+    circ_vals2 = _/np.linalg.norm(_)
 
+    cross_cor = np.fft.ifft(np.fft.fft(circ_vals1, axis=1) *
+                        np.fft.fft(np.fliplr(circ_vals2), axis=1),
+                        axis=1)
+    sum_over_r = np.sum(np.real(cross_cor),axis=0)
+
+    return sum_over_r, angles
+
+#os.chdir('/Users/deanpospisil/Desktop/')
+#im=mpimg.imread('soft_on_orig.png')
+#im = np.sum(im, 2)
+
+
+sample_rate_mult=1
 ft = np.zeros((100,51))
 ft[10, 0] = 1
 im1 = np.fft.irfft2(ft)
 ft = np.zeros((100,51))
 ft[0, 10] = 1
 im2 = np.fft.irfft2(ft)
+sum_over_r, angles = circ_cor(im1, im2, sample_rate_mult=sample_rate_mult)
+
 
 plt.subplot(511)
 plt.imshow(im1, interpolation='None',cmap = cm.Greys_r)
+plt.gca().set_yticks([])
+plt.gca().set_xticks([])
 
 plt.subplot(512)
 plt.imshow(im2, interpolation='None',cmap = cm.Greys_r)
+plt.gca().set_yticks([])
+plt.gca().set_xticks([])
 
 plt.subplot(513)
 circ_vals1, angles = cart_to_polar_2d_lin(im1, sample_rate_mult)
-#circ_vals1, angles = cart_to_polar_2d_fft(im1, sample_rate_mult)
 circ_vals1 = circ_vals1/np.linalg.norm(circ_vals1)
-
-
 plt.imshow(circ_vals1, interpolation='None',cmap = cm.Greys_r)
-
+plt.gca().set_yticks([])
+plt.gca().set_xticks([])
 
 plt.subplot(514)
 circ_vals2, angles = cart_to_polar_2d_lin(im2, sample_rate_mult)
-#circ_vals2, angles = cart_to_polar_2d_fft(im2, sample_rate_mult)
-
 circ_vals2 = circ_vals2/np.linalg.norm(circ_vals2)
 plt.imshow(circ_vals2, interpolation='None',cmap = cm.Greys_r)
+plt.gca().set_yticks([])
+plt.gca().set_xticks([])
 
-
-cross_cor = np.fft.ifft(np.fft.fft(circ_vals1, axis=1) * 
-                        np.fft.fft(np.fliplr(circ_vals2), axis=1), 
-                        axis=1)
 plt.subplot(515)
-sum_over_r = np.sum(np.real(cross_cor),axis = 0)
-#plt.imshow( np.real(cross_cor), interpolation='None',cmap = cm.Greys_r)
 plt.plot(np.rad2deg(angles), sum_over_r)
 
 print(np.rad2deg(angles[np.argmax(sum_over_r)]))
-
-#circ_vals, x, y = cart_to_polar_2d_fft(im)
-
-#plt.figure()
-#put them back into a square, so we can plot it
-
-#plt.subplot(121)
-#plt.imshow(np.real(im), interpolation='None',cmap = cm.Greys_r)#just get real part because I didn't bother with symmetry
-#plt.gca().set_xticks([])
-#plt.gca().set_yticks([])
-#
-#plt.scatter((xnew)*im.shape[0], (ynew)*im.shape[0], s=0.1)
-#plt.gca().set_aspect('equal', adjustable='box')
-#plt.gca().set_xticks([])
-#plt.gca().set_yticks([])
-#
-#plt.subplot(122)
-#plt.imshow(np.real(circ_vals), interpolation='None',cmap = cm.Greys_r)
-#plt.xlabel('Orientation')
-#plt.ylabel('Lin(r)')
-#plt.gca().set_xticks([])
-#plt.gca().set_yticks([])
-
--- misc/figure_out_organize_netwts.py
+++ misc/figure_out_organize_netwts.py
@@ -6,7 +6,86 @@ Created on Thu Mar 17 20:12:06 2016
 """
 
 import pickle
-import sys  
- 
-with open('/Users/deanpospisil/Desktop/modules/net_code/nets/netwts.p', 'rb') as f:
-    a = pickle.load(f, encoding='latin1')
+import os, sys
+import numpy as np
+from scipy import interpolate
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+top_dir = os.getcwd().split('net_code')[0]
+sys.path.append(top_dir + 'xarray')
+top_dir = top_dir+ 'net_code/'
+sys.path.append(top_dir)
+sys.path.append(top_dir + 'common')
+import xarray as xr
+
+
+
+with open(top_dir + 'nets/netwts.p', 'rb') as f:
+    a = pickle.load(f)
+
+def cart_to_polar_2d_lin(im, sample_rate_mult):
+
+    x = np.arange(im.shape[1])
+    y = np.arange(im.shape[0])
+    f = interpolate.interp2d(x, y, im, kind='linear')
+
+    #get polar resampling coordinates
+    n_pix = np.size(im,0)/2.
+    npts_mag = int(np.size(im, 0) / 2.)*sample_rate_mult
+    npts_angle = int(np.pi * 2 * n_pix)*sample_rate_mult
+
+    angles_vec = np.linspace(0, 2*np.pi, npts_angle)
+    magnitudes_vec = np.linspace(0, n_pix-1, npts_mag)
+    angles, magnitudes = np.meshgrid(angles_vec, magnitudes_vec)
+    xnew = (magnitudes * np.cos(angles)+n_pix).ravel()
+    ynew = (magnitudes * np.sin(angles)+n_pix).ravel()
+    f = interpolate.RegularGridInterpolator((x, y), im, method='linear')
+
+    pts = np.fliplr(np.array([xnew, ynew]).T)
+    im_pol = f(pts).reshape(npts_mag, npts_angle)
+    im_pol = im_pol * magnitudes*2*np.pi
+
+    return im_pol
+
+def cart_to_polar_2d_lin_broad(im, sample_rate_mult):
+    cut = [cart_to_polar_2d_lin(im_cut, sample_rate_mult) for im_cut in  im]
+    return cut
+
+def circ_cor(pol1, pol2, sample_rate_mult=2):
+
+
+    cross_cor = np.fft.ifft(np.fft.fft(pol1, axis=1) *
+                        np.fft.fft(np.fliplr(pol2), axis=1),
+                        axis=1)
+    sum_over_r = np.sum(np.real(cross_cor), axis=0)
+
+    return sum_over_r, angles
+
+# convert these to circular
+ims = a[0][1]
+ims = np.array([im for im in ims])
+
+
+sample_rate_mult = 1
+pol_ims = np.array([cart_to_polar_2d_lin_broad(im, sample_rate_mult) for im in ims])
+pol_ims = pol_ims / np.sqrt(np.sum(pol_ims**2, axis=(1,2,3), keepdims=True))
+pol_ims = np.fft.fft(pol_ims)
+
+p3 = np.expand_dims(pol_ims, 0) * np.conj(np.expand_dims(pol_ims, 1))
+
+p4 = np.fft.ifft(p3)
+p4 = np.sum(p4, axis = (2, 3))
+cormat=np.real(np.max(abs(p4), 2))
+#plt.plot(np.diag(cormat))
+
+plt.imshow(np.real(np.max(abs(p4), 2)), interpolation='None', cmap=cm.Greys_r)
+plt.colorbar()
+plt.title('rotational correlation, AlexNet Kernels Layer 1')
+plt.xlabel('unit #')
+plt.ylabel('unit #')
+'''
+for ind in range(len(pol_ims)):
+    plt.subplot(10,10,ind+1)
+    plt.imshow(pol_ims[ind][0],interpolation='None', cmap=cm.Greys_r)
+    '''

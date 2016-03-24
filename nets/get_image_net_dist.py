b4e7eb642970b54792066c4bbded7e2ede25b619
Dean Pospisil <dpospisi@students.pitzer.edu>
2016-03-23 17:44:08 -0700

    get nat image dist working

--- nets/get_image_net_dist.py
+++ nets/get_image_net_dist.py
@@ -57,7 +57,6 @@ transformer.set_transpose('data', (2,0,1))  # move image channels to outermost d
 transformer.set_mean('data', mu)            # subtract the dataset-mean value in each channel
 transformer.set_raw_scale('data', 255)      # rescale from [0, 1] to [0, 255]
 transformer.set_channel_swap('data', (2,1,0))  # swap channels from RGB to BGR
-
 image = caffe.io.load_image(caffe_root + 'examples/images/cat.jpg')
 transformed_image = transformer.preprocess('data', image)
 #plt.imshow(image)
@@ -67,7 +66,7 @@ transformed_image = transformer.preprocess('data', image)
 net.blobs['data'].reshape(50,        # batch size
                           3,         # 3-channel (BGR) images
                           227, 227)  # image size is 227x227
-
+                          
 # copy the image data into the memory allocated for the net
 net.blobs['data'].data[...] = transformed_image
 
@@ -76,4 +75,4 @@ output = net.forward()
 
 output_prob = output['prob'][0]  # the output probability vector for the first image in the batch
 
-print 'predicted class is:', output_prob.argmax()
+print 'predicted class is:', output_prob.argmax()                          
